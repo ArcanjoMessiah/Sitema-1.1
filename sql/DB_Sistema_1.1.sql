d558119ff30de8286698bac923887128d5f4305e
Douglas <arcanjomessiah@live.com>
2023-11-05 18:47:16 -0300

    Correção de arquivo SQL

--- sql/DB_Sistema_1.1.sql
+++ sql/DB_Sistema_1.1.sql
@@ -3,10 +3,6 @@
 -- Model: New Model    Version: 1.0
 -- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
